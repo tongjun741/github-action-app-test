cc2949caefe1720655164c6068c30a32d1f4028c
童俊 <tongjun741@126.com>
2024-01-09 21:15:49 +0800

    测试自动启动

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,39 +1,17 @@
-const { _electron: electron } = require('playwright');
-const { test, expect } = require('@playwright/test');
+describe('Electron App Test', () => {
+  it('should open Electron app and perform actions', async  () => {
 
-let window, electronApp;
+      await $('div[class*=app-version]').waitForExist({ timeout: 10*1000 })
 
-test.beforeAll(async () => {
-  // Launch Electron app.
-  electronApp = await electron.launch({ args: ['main.js'] });
+      
+      // 验证页面标题
+      const title = await browser.getTitle();
+      console.log("标题是", title);
+      // expect(title).toBe('Your Electron App Title');
 
-  // Evaluation expression in the Electron context.
-  const appPath = await electronApp.evaluate(async ({ app }) => {
-    // This runs in the main Electron process, parameter here is always
-    // the result of the require('electron') in the main app script.
-    return app.getAppPath();
+      const version = await $('div[class*=app-version]').getText();
+      console.log("版本号是：", version);
+
+      // expect(version).toBe('Clicked');
   });
-  console.log(appPath);
-
-  // Get the first window that the app opens, wait if necessary.
-  window = await electronApp.firstWindow();
-  // Print the title.
-  console.log(await window.title());
-  // Capture a screenshot.
-  await window.screenshot({ path: 'tests/screenshots/out.png' });
-  // Direct Electron console to Node terminal.
-  window.on('console', console.log);
-
 });
-
-test('window has correct title', async () => {
-  const title = await window.title()
-  expect(title).toBe('Hello World!')
-})
-
-test.afterAll(async () => {
-  // Exit app.
-  await electronApp.close();
-});
-
-// 
