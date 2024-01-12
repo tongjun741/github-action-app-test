f8ae22c87d3569e1f684a92a2be309ce91eb49bd
童俊 <tongjun741@126.com>
2024-01-13 00:21:51 +0800

    指定启动路径

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,12 @@
 const path = require('path');
+const os = require('os');
+
+let exePath = path.join(process.env.USERPROFILE, 'AppData', 'Local', 'Programs', 'HuaYoung', '花漾客户端.exe');
+if (os.platform() === 'darwin') {
+    exePath = "/Applications/花漾客户端.app/Contents/MacOS/花漾客户端";
+} else if (os.platform() === 'linux') {
+    exePath = "/opt/花漾客户端/huayoung";
+}
 
 exports.config = {    //
     // ====================
