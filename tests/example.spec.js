8f4f65ba3f9a773eae9318ff7a7bfc439fa41be5
童俊 <tongjun741@126.com>
2024-01-15 22:35:50 +0800

    登录及打开会话

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,17 +1,66 @@
+const sleep = (ms = 0) => new Promise((r) => setTimeout(r, ms));
+
 describe('Electron App Test', () => {
-  it('should open Electron app and perform actions', async  () => {
+  it('should open Electron app and perform actions', async () => {
 
-      await $('div[class*=app-version]').waitForExist({ timeout: 10*1000 })
+    await $('div[class*=app-version]').waitForExist({ timeout: 10 * 1000 })
 
-      
-      // 验证页面标题
-      const title = await browser.getTitle();
-      console.log("标题是", title);
-      // expect(title).toBe('Your Electron App Title');
+    // 验证页面标题
+    let title = await browser.getTitle();
+    console.log("标题是", title);
+    // expect(title).toBe('Your Electron App Title');
 
-      const version = await $('div[class*=app-version]').getText();
-      console.log("版本号是：", version);
+    const version = await $('div[class*=app-version]').getText();
+    console.log("版本号是：", version);
 
-      // expect(version).toBe('Clicked');
+    // expect(version).toBe('Clicked');
+    await $('//span[text()="童1227"]').click();
+
+    await sleep(3000);
+    let windowIDs = await browser.getWindowHandles();
+    console.log(windowIDs);
+    browser.switchToWindow(windowIDs[0])
+    await sleep(3000);
+    let mainWindowID = await browser.getWindowHandle();
+    console.log(mainWindowID);
+
+    await $('//a[text()="抖音_TJ"]').waitForExist({ timeout: 10 * 1000 })
+    title = await browser.getTitle();
+    console.log("标题是", title);
+
+    await $('//a[text()="抖音_TJ"]').click();
+    await $('//span[text()="打开浏览器"]').waitForExist({ timeout: 10 * 1000 })
+    await $('//span[text()="打开浏览器"]').click();
+
+    while (true) {
+      await sleep(3000);
+      windowIDs = await browser.getWindowHandles();
+      console.log(windowIDs);
+      let newWindow = windowIDs.find(o => o !== mainWindowID)
+      if (windowIDs.length>1) {
+        for(let i=0;i<windowIDs.length;i++){
+          browser.switchToWindow(newWindow);
+          await sleep(3000);
+          title = await browser.getTitle();
+          console.log("标题是", title);
+        }
+        // break;
+      }
+    }
+    title = await browser.getTitle();
+    console.log("标题是", title);
+
+    await $('//a[text()="定制浏览器首页"]').waitForExist({ timeout: 10 * 1000 })
+    title = await browser.getTitle();
+    console.log("标题是", title);
+
+    await browser.newWindow('https://webdriver.io', {
+      windowName: 'WebdriverIO window',
+      windowFeature: 'width=420,height=230,resizable,scrollbars=yes,status=1',
+    });
+    
+    console.log(await browser.getTitle()) // outputs: "WebdriverIO · Next-gen browser and mobile automation test framework for Node.js"
+
+
   });
 });
