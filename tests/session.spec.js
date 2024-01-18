32f69a89302dd69fd2ea0ff7a059472dab8168c0
童俊 <tongjun741@126.com>
2024-01-18 14:05:47 +0800

    测试打开会话

---
+++ tests/session.spec.js
@@ -0,0 +1,26 @@
+
+describe('Electron App Test', () => {
+    it('should open Electron app and perform actions', async () => {
+        const browser = await remote({
+            logLevel: 'trace',
+            automationProtocol: 'devtools',
+            capabilities: {
+                browserName: 'chrome',
+                'goog:chromeOptions': {
+                    port: 8041,
+                    args: [
+                        '--remote-debugging-host=127.0.0.1',
+                        '--remote-debugging-port=9222'
+                    ]
+                }
+            }
+        });
+
+
+        // 验证页面标题
+        let title = await browser.getTitle();
+        console.log("标题是", title);
+
+    });
+
+});
