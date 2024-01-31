2e4313283503aefb8595ebebfd58d123c36708d7
童俊 <tongjun741@126.com>
2024-01-31 17:30:44 +0800

    输出标准输出

--- win7/cmd.js
+++ win7/cmd.js
@@ -9,13 +9,33 @@ try {
 
   // 下载.exe文件
   console.log('开始下载.exe文件...');
-  execSync(`powershell -Command "(New-Object Net.WebClient).DownloadFile(\'${exeUrl}\', \'${exeName}\')"`);
-  console.log('.exe文件下载完成！');
+  try {
+    const stdout = execSync(`powershell -Command "(New-Object Net.WebClient).DownloadFile(\'${exeUrl}\', \'${exeName}\')"`);
+    console.log('.exe文件下载完成！');
+    console.log('标准输出:', stdout.toString());
+  } catch (error) {
+    console.error('下载文件时发生错误:', error.stderr.toString());
+  }
 
   // 安装.exe文件
   console.log('开始安装.exe文件...');
-  execSync(`powershell -Command "Start-Process -FilePath \'${exeName}\' -ArgumentList \'/S\' -Wait"`);
-  console.log('.exe文件安装完成！');
+  try {
+    const stdout = execSync(`powershell -Command "Start-Process -FilePath \'${exeName}\' -ArgumentList \'/S\' -Wait"`);
+    console.log('.exe文件安装完成！');
+    console.log('标准输出:', stdout.toString());
+  } catch (error) {
+    console.error('安装时发生错误:', error.stderr.toString());
+  }
+
+  // 开始测试
+  console.log('开始测试');
+  try {
+    const stdout = execSync(`nmp install yarn -g && yarn && yarn wdio`);
+    console.log('测试完成！');
+    console.log('标准输出:', stdout.toString());
+  } catch (error) {
+    console.error('测试时发生错误:', error.stderr.toString());
+  }
 } catch (e) {
   console.error(e);
 } finally {
