3e869d913d94e2ca726fc425fa4972a17228d577
童俊 <tongjun741@126.com>
2024-01-31 16:53:11 +0800

    系统启动时需要执行cmd.js

--- win7/start.js
+++ win7/start.js
@@ -7,10 +7,7 @@ let lastContent = null;
 function checkFile() {
   try {
     const content = fs.readFileSync(filePath, 'utf8');
-    if (lastContent === null) {
-      // 初次运行，记录文件内容
-      lastContent = content;
-    } else if (content !== lastContent) {
+    if (content !== lastContent) {
       // 文件内容已变化，执行命令
       lastContent = content;
       console.log(`${filePath} has been modified, executing...`);
