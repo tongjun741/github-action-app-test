4b5ed7d29c33fe45f8c8caa15541ba2a74fa1a79
童俊 <tongjun741@126.com>
2024-02-03 10:36:38 +0800

    添加配置

---
+++ tests/config.js
@@ -0,0 +1,13 @@
+let devConfig = {
+    username: '945b33aa5652@drmail.in',
+    password: '#e$*JWc=yKC%YHnc',
+    teamName: '945b33的团队',
+    shopName: 'demo-1',
+};
+
+let productConfig = {
+    username: '8606d3fbe819@drmail.in',
+    password: 'itX~9M=_2wUBxr01',
+    teamName: '用户8606的团队',
+    shopName: 'demo-1',
+};
