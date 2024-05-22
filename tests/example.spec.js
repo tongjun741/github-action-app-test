502e445432fa085bf51edc208978ecfb43a2c0bd
童俊 <tongjun741@126.com>
2024-05-22 19:52:34 +0800

    修正变量名错误

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -11,12 +11,12 @@ describe('Electron App Test', () => {
     await $(`.icon-chrome_outline`).waitForExist({ timeout: 10 * 1000 })
     await $(`.icon-chrome_outline`).click();
 
-    await $(`//a[text()="${config.shopName}"]`).waitForExist({ timeout: 10 * 1000 })
+    await $(`//a[text()="${productConfig.shopName}"]`).waitForExist({ timeout: 10 * 1000 })
     title = await browser.getTitle();
     console.log("标题是", title);
 
     // 进入分身详情页面
-    await $(`//a[text()="${config.shopName}"]`).click();
+    await $(`//a[text()="${productConfig.shopName}"]`).click();
     // 打开浏览器
     await $('//span[text()="打开浏览器"]').waitForExist({ timeout: 10 * 1000 });
     await $('//span[text()="打开浏览器"]').click();
