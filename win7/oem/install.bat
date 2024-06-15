93fcdeeca425b74d67e7bfcf160c561396eba5ab
童俊 <tongjun741@126.com>
2024-06-15 15:09:04 +0800

    更换win7下发送请求的方式

--- win7/oem/install.bat
+++ win7/oem/install.bat
@@ -1,15 +1,22 @@
 @echo off
 
 REM 获取当前时间，并格式化为 "HHMMSS" 格式
-set currentTime=%time:~0,2%%time:~3,2%%time:~6,2%
+for /f "tokens=1-3 delims=:." %%a in ("%TIME%") do (
+    set hh=%%a
+    set mm=%%b
+    set ss=%%c
+)
 
 REM 去掉时间中的空格（如果小时是个位数，比如 9 点）
-set currentTime=%currentTime: =0%
+if %hh% lss 10 set hh=0%hh%
+if %mm% lss 10 set mm=0%mm%
+if %ss% lss 10 set ss=0%ss%
+
+set currentTime=%hh%%mm%%ss%
 
 REM 替换 URL 中的 "newline2" 为当前时间
 set url=http://ds.0728123.xyz:65080/log_channel12?text=%currentTime%
 
-echo "%url%"
+REM 使用 bitsadmin 发送请求
+bitsadmin /transfer myDownloadJob /download /priority normal "%url%" "%CD%\response.txt"
 
-REM 使用 curl 发送请求
-curl "%url%"
