aa5da9cb41cb6ab3c97b9aee2f8c2f1c9b88cda0
童俊 <tongjun741@126.com>
2024-06-19 14:12:35 +0800

    等待网络共享文件夹就绪

--- win7/shared/start.bat
+++ win7/shared/start.bat
@@ -1,6 +1,19 @@
 set NODE_SKIP_PLATFORM_CHECK=1
 c:\node\node -v
 
+REM 等待网络共享文件夹就绪
+set SHARE_PATH=\\host.lan\Data
+
+:CheckShare
+ping -n 1 %SHARE_PATH% >nul
+if errorlevel 1 (
+    echo Network share %SHARE_PATH% is not accessible. Waiting 10 seconds...
+    timeout /t 10 >nul
+    goto CheckShare
+)
+
+echo Network share %SHARE_PATH% is accessible.
+
 cd c:\work
 c:\node\node c:\work\cmd.js > \\host.lan\Data\start.log
 echo "start.bat over" >> "\\host.lan\Data\done.log"
