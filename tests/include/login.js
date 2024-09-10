d5d23cb886c801f6f05941d305c8dafde93e32e9
童俊 <tongjun741@126.com>
2024-09-10 18:56:19 +0800

    修改日志

--- tests/include/login.js
+++ tests/include/login.js
@@ -1,14 +1,15 @@
-const { sleep } = require('./tools');
+const { sleep, outputLog } = require('./tools');
 
 async function login(config, password, targetBrowser) {
   if (targetBrowser) {
     browser = targetBrowser;
   }
-  
+
   while (true) {
     try {
       // 按标题切换到主窗口
       // 兼容有首页和默认只有分身页的情况
+      outputLog("按标题切换到主窗口，兼容有首页和默认只有分身页的情况");
       await browser.switchWindow(' - 花漾灵动');
       break;
     } catch (e) {
@@ -19,14 +20,16 @@ async function login(config, password, targetBrowser) {
   await browser.$('div[class*=app-version]').waitForExist({ timeout: 100 * 1000 })
 
   // 验证页面标题
+  outputLog("验证页面标题");
   let title = await browser.getTitle();
-  console.log("标题是", title);
+  outputLog(`当前窗口标题是${title}`);
   // expect(title).toBe('Your Electron App Title');
 
   const version = await browser.$('div[class*=app-version]').getText();
-  console.log("版本号是：", version);
+  outputLog("版本号是：", version);
 
   // 检查当前是登录页面还是团队选择界面
+  outputLog("检查当前是登录页面还是团队选择界面");
   await browser.waitUntil(async () => {
     return await browser.$('//div[text()="邮箱登录"]').isExisting() ||
       await browser.$(`//span[text()="${config.teamName}"]`).isExisting();
@@ -36,15 +39,17 @@ async function login(config, password, targetBrowser) {
   });
 
   // 等待10s确认服务器是否可用
+  outputLog("等待10s确认服务器是否可用");
   await sleep(10 * 1000);
   // 服务器不可用，等待服务器恢复
   while (await browser.$('//div[contains(concat(" ", normalize-space(@class), " "), " marquee-container ")]//span[contains(., "请检查您的网络是否通畅")]').isExisting()) {
-    console.log("服务器不可用，等待服务器恢复")
+    outputLog("服务器不可用，等待服务器恢复")
     await sleep(60 * 1000);
   }
 
   // 当前是登录页面，开始登录
   if (await browser.$('//div[text()="邮箱登录"]').isExisting()) {
+    outputLog("当前是登录页面，开始登录")
     await browser.$(`//div[text()="邮箱登录"]`).click();
     await browser.$('#account').setValue(config.username);
     await browser.$('#password').setValue(password);
@@ -60,6 +65,7 @@ async function login(config, password, targetBrowser) {
 
     try {
       // 按标题切换到主窗口
+      outputLog("按标题切换到主窗口")
       // 兼容有首页和默认只有分身页的情况
       await browser.switchWindow(' - 花漾灵动');
       break;
@@ -67,7 +73,7 @@ async function login(config, password, targetBrowser) {
     }
   }
   title = await browser.getTitle();
-  console.log("标题是", title);
+  outputLog(`登录完成，当前窗口标题是${title}`);
 };
 
 module.exports = login;
