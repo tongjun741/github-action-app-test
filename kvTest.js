51dbae66ad58ece7bd357928954d8fc2d485dfd9
童俊 <tongjun741@126.com>
2024-09-10 13:42:32 +0800

    修改测试的默认key

--- kvTest.js
+++ kvTest.js
@@ -6,7 +6,7 @@ async function main(){
     console.log(s);
     let newTime = new Date().getTime();
     console.log(newTime);
-    await saveResult(true, 'w', 'dd'+newTime);
+    await saveResult(true, 'Windows 10', 'dd'+newTime);
     s = await showResultTable(true);
     console.log(s);
 }
