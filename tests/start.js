704e631cd24260c68f2218f4ee86a1a1316c3ef8
童俊 <tongjun741@126.com>
2024-09-30 19:16:58 +0800

    macOS客户端截图有bug，先忽略

--- tests/start.js
+++ tests/start.js
@@ -61,6 +61,7 @@ async function main() {
   let appScreenshotUrl = "客户端截图失败";
   if(process.env.E2E_PLATFORM && process.env.E2E_PLATFORM.indexOf('macOS')>-1){
     outputLog("macOS客户端截图有bug，先忽略");
+    appScreenshotUrl = "macOS客户端截图有bug，先忽略";
   }else{
     try {
       // 对客户端截图
