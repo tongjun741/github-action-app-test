c69e3ebff79ae66d3b40ecafde8daac1ef72c581
童俊 <tongjun741@126.com>
2025-08-16 18:59:30 +0800

    更新密码

--- macOS_VNC/start.sh
+++ macOS_VNC/start.sh
@@ -7,8 +7,8 @@ sudo dscl . -create /Users/runneradmin RealName Runner_Admin
 sudo dscl . -create /Users/runneradmin UniqueID 1001
 sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
 sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
-sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
-sudo dscl . -passwd /Users/runneradmin P@ssw0rd!
+sudo dscl . -passwd /Users/runneradmin p123456
+sudo dscl . -passwd /Users/runneradmin p123456
 sudo createhomedir -c -u runneradmin > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership runneradmin
 #Enable VNC
